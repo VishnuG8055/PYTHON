fc933f4992693afee4a1f4004f7502a9629a2223
VISHNU VARDHAN G <153700555+VishnuG8055@users.noreply.github.com>
2024-08-11 18:36:03 +0530

    Update 03_type_casting.py

--- PYTHON COURSE/1 BASICS/03_type_casting.py
+++ PYTHON COURSE/1 BASICS/03_type_casting.py
@@ -1,13 +1,13 @@
 #TYPE FUNCTION - type()
-name = "Bro"
-age = 21
-gpa = 1.9
+name = "Vishnu"
+age = 20
+gpa = 9.4
 student = True
 
-print(type(name))
-print(type(age))
-print(type(gpa))
-print(type(student)) 
+print(type(name))     # <class 'str'>
+print(type(age))      # <class 'int'>
+print(type(gpa))      # <class 'float'>
+print(type(student))  # <class 'bool'>
 
 # -------------------------------
 #         TYPE CASTING
@@ -20,46 +20,63 @@ print(type(student))
 
 # EXPLICIT TYPE CASTING
 age = 21
-age = float(age)
-print(age)
-print(type(age))
+age = float(age)  
+print(age)         # 21.0
+print(type(age))   # <class 'float'>
 
-gpa =3.9999
+gpa = 9.9999
 gpa = int(gpa)
-print(gpa)
+print(gpa)          # 9
 
-student = str(student)
-print(student)
-name = 21
-name = bool(name)
-print(name)  #True
+student = True
+student = str(student) 
+print(student)      # True
+age = 21
+age = bool(age)
+print(age)         # True
 
 a = 0
 a = bool(a)
-print(a)  #False
+print(a)      # False
 b = ""
 b = bool(b)
-print(b)  #False
+print(b)      # False
 
 s = "vishnu"
 print(list(s))
 print(tuple(s))
 print(set(s))
 
+# Output- 
+# ['v', 'i', 's', 'h', 'n', 'u']
+# ('v', 'i', 's', 'h', 'n', 'u')
+# {'u', 'h', 'n', 'i', 's', 'v'}
+
 a = 20
 print(bin(a))
 print(hex(a))
 print(oct(a))
 
+# Ouput- 
+# 0b10100
+# 0x14
+# 0o24
+
+#---------------------------
 # IMPLICIT TYPE CASTING
+#---------------------------
 
 x = 2
 y = 2.5
-x = x/y
-print(x)
-print(type(x))
-print(x+y)
+z = 3
+x = x/y           # Since y is a float, the result of the division will be a float
+print(x)          # 0.8
+print(type(x))    # <class 'float'>
+print(z+y)        # 5.5
 
 a = 5
 b = True
 print(a+b) # 6
+
+
+#-------------------------
