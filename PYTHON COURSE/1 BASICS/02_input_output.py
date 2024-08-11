f94c871650556b79d05405edd1049b6eec580aa6
VISHNU VARDHAN G <153700555+VishnuG8055@users.noreply.github.com>
2024-08-11 18:25:47 +0530

    Update 02_input_output.py

--- PYTHON COURSE/1 BASICS/02_input_output.py
+++ PYTHON COURSE/1 BASICS/02_input_output.py
@@ -7,12 +7,23 @@
 print("Hello")
 print("welcome","to","python")
 
+# Output -
+# Hello
+# welcome to python
+
 # end and sep parameters in print()
-# end - Customization for continuous output 
+# end - Customization for continuous output
 print("welcome",end =" ")
 print("to python")
+
+# Output- 
+# welcome to python
+
 # sep - Seperator used between multiple arguments
 print("10","08","2004",sep = ".")
+
+# Output - 
+# 10.08.2004
 
 # -------------------------------
 #             INPUT
@@ -20,12 +31,13 @@ print("10","08","2004",sep = ".")
 # input() - This function is used to take input from user
 # input() always gives us a string 
 # so need to type cast it
+
 name = input("ENTER NAME :")
 age = input("Enter age : ")
 age = age + 1 
 
-print(age)  # Error
-print(f"Name : {name}")
+print(age)  # TypeError
+print(f"Name : {name}")  
 
 x = int(input("x = "))
 y = int(input("y = "))
