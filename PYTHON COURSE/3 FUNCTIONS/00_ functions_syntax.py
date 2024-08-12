818a69a385a8c0f29c7941cf738bb1636550145d
VISHNU VARDHAN G <153700555+VishnuG8055@users.noreply.github.com>
2024-08-12 20:16:34 +0530

    f3jn

---
+++ PYTHON COURSE/3 FUNCTIONS/00_ functions_syntax.py
@@ -0,0 +1,21 @@
+# A function is a block of code which only runs when it is called.
+# In Python a function is defined using the def keyword
+
+def my_function():
+  print("Hello")
+
+# Calling a Function - To call a function, use the function name followed by parenthesis
+
+my_function()
+
+
+# Function with arguments
+
+Name = "Vishnu"
+roll = 810
+
+def Student(x,y):
+    print(f"Your Name is {x} and roll is {y}")
+
+Student(Name,roll)
+
